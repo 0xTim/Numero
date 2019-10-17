7df1925cc3a5c408d5fe6acdcff2ddabe5f8924c
Tim <0xtimc@gmail.com>
2019-10-17 16:15:05 +0100

    Remove ci.sh for video

--- ci.sh
+++
@@ -1,2 +0,0 @@
-xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'platform=iOS Simulator,name=iPhone 8' test
-xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'generic/platform=iOS' -configuration Release build CODE_SIGNING_ALLOWED=NO
