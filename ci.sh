083e96165c145f081038565f2d530c987b583866
Tim <0xtimc@gmail.com>
2019-10-17 17:40:05 +0100

    Break the converter

--- ci.sh
+++ ci.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+set -e
 xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'platform=iOS Simulator,name=iPhone 8' test
 xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination generic/platform=iOS -configuration Release build CODE_SIGNING_ALLOWED=NO
